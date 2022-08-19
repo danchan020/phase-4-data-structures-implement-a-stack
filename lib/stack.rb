8898d2925bb67e3fa9e69ebe0e7f807531688ea8
Daniel <danchan020@gmail.com>
2022-08-19 14:01:06 -0700

    commit tree d21f7756d39a9b9d2c189faf205e02a1c36a9a25

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,34 @@
-# your code here
+class Stack
+    def initialize
+        @stack =[]
+    end
+
+    
+    def pop
+        @stack.pop
+    end
+    
+    def peek
+        @stack.peek
+    end
+    
+    # def size
+    #     @stack.size
+    # end
+    
+    # def empty?
+    #     @stack.empty?
+    # end
+    
+    def full?
+        @stack.full?
+    end
+    
+    def push(value)
+        @stack.push(value) 
+    end
+
+    # def search(value)
+    #     @stack.search(value)
+    # end
+end 
